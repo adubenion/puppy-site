268f9befa32f7abb1cc0d8b2013efd388a6c4900
adubenion <aaron.dubenion@gmail.com>
2017-11-02 00:15:11 -0400

    edit files

--- src/js/Photos.jsx
+++ src/js/Photos.jsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import React, {setState} from 'react';
+import PropTypes from 'prop-types';
+
+class Gallery extends React.Component {
+	render() {
+		return(
+			<div>
+				<img src={'./img/prod/' + this.props.propArray[this.props.photo]} />
+			</div>
+		);
+	}
+}
+
+Gallery.propTypes = {
+
+	propArray: PropTypes.array,
+	photos: PropTypes.number
+}
+
+Gallery.defaultProps = {
+	photo: "",
+
+	propArray: [
+		"black-white-dog.png",
+		"corgie.png",
+		"lap-dog.png",
+		"puppy-in-grass.png",
+		"two-dogs.png"
+	]
+}
 
 
 export class Photos extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			number: 0
+		}
+
+		this.stateIncreaseHandler = this.stateIncreaseHandler.bind(this);
+		this.stateDecreaseHandler = this.stateDecreaseHandler.bind(this);
+	}
+
+	stateIncreaseHandler() {
+		var upNumber = this.state.number
+
+		if (upNumber >= 3) {
+			upNumber = 3
+			return this.setState({number: upNumber})
+		} else {
+			upNumber += 1
+			return this.setState({number: upNumber})
+		}			
+	}
+
+	stateDecreaseHandler() {
+		var downNumber = this.state.number
+
+		if (downNumber <= 0) {
+			downNumber = 0
+			return this.setState({number: downNumber})
+		} else {
+			downNumber -= 1
+			return this.setState({number: downNumber})
+		}
+	}
+
 	render() {
 		return (
 			<div>
@@ -12,8 +75,8 @@ export class Photos extends React.Component {
 					</div>
 					<div className="col-mid">
 						<h3>Pictures</h3>
-							
-						<button id="prev">Prev</button><button id="next">Next</button>
+							<Gallery photo={this.state.number} />
+						<button onClick={this.stateDecreaseHandler} id="prev">Prev</button><button onClick={this.stateIncreaseHandler} id="next">Next</button>
 					</div>
 					<div className="col">
 						<h3>column</h3>
